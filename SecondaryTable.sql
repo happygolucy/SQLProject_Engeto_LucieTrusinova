f383f8c714767b23fd5434440d94c8368218fb1c
happygolucy <lucie.trusinova@gmail.com>
2026-09-01 21:32:27 +0200

    Update SecondaryTable.sql

--- SecondaryTable.sql
+++ SecondaryTable.sql
@@ -1,21 +1,22 @@
 --tvorba sekundarní tabulky
 
-
-CREATE TABLE t_lucie_trusinova_project_sql_secondary_final
-	(id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+CREATE TABLE t_lucie_trusinova_project_sql_secondary_final (
+	id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
  	country text NOT NULL,    
-    YEAR INT NOT NULL,    
+    year INT NOT NULL,    
     gdp NUMERIC(15, 2),
- 	  gini NUMERIC (12, 2),
+ 	gini NUMERIC (12, 2),
   	population NUMERIC (12, 2));
 
 --insert do sekundární tabulky
 
-INSERT
-	INTO
-	t_lucie_trusinova_project_sql_secondary_final
-	(country, YEAR,	gdp, gini, population)
-
+INSERT INTO t_lucie_trusinova_project_sql_secondary_final (
+	country,
+	year,
+	gdp,
+	gini,
+	population
+	)
 SELECT
 	c.country,
 	e.year,
@@ -24,9 +25,9 @@ SELECT
 	e.population
 FROM
 	countries c
-JOIN economies e ON
-	c.country = e.country
+JOIN economies e
+	ON c.country = e.country
 WHERE
 	c.continent = 'Europe'
 	AND e.year BETWEEN 2006 AND 2018
-ORDER BY country, YEAR;
+ORDER BY country, year;
