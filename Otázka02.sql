a8be9966da4b50c22e280f90e0f93beaf5f59f81
happygolucy <lucie.trusinova@gmail.com>
2026-09-01 21:21:27 +0200

    Update Otázka02.sql

--- Otázka02.sql
+++ Otázka02.sql
@@ -3,27 +3,27 @@
 
 WITH avg_wages AS (
     SELECT
-        YEAR,
+        year,
         ROUND(AVG(wages), 2) AS avg_wage
     FROM t_lucie_trusinova_project_sql_primary_final
     WHERE wages IS NOT NULL
-    GROUP BY YEAR
+    GROUP BY year
 )
 SELECT
-    p.YEAR,
+    p.year,
     p.food_type,
     p.price_level,
     w.avg_wage,
     ROUND(w.avg_wage / p.price_level, 0) AS quantity_can_buy
 FROM t_lucie_trusinova_project_sql_primary_final p
 JOIN avg_wages w
-    ON p.YEAR = w.YEAR
+    ON p.year = w.year
 WHERE p.food_type IN (
         'Mléko polotučné pasterované',
         'Chléb konzumní kmínový'
     )
-    AND p.YEAR IN (2006, 2018)
+    AND p.year IN (2006, 2018)
 ORDER BY
     p.food_type,
-    p.YEAR;
+    p.year;
 
