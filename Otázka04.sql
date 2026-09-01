4b83fc0bba0f65bf22088a014d60696db6950e30
happygolucy <lucie.trusinova@gmail.com>
2026-09-01 21:26:18 +0200

    Update Otázka04.sql

--- Otázka04.sql
+++ Otázka04.sql
@@ -1,38 +1,37 @@
-
 --podhled 1 pro ukol 4 (Průměrná cena a mzda za jednotlivé roky)
 
 CREATE VIEW question4answerdata_Lucie_Trusinova1 AS
 SELECT
-    YEAR,
+    year,
     ROUND(AVG(price_level), 2) AS avg_price,
     ROUND(AVG(wages), 2) AS avg_wages
 FROM t_lucie_trusinova_project_sql_primary_final
-GROUP BY YEAR
-ORDER BY YEAR;
+GROUP BY year
+ORDER BY year;
 
 --podhled 2 pro ukol 4 (Meziroční vývoj):
 
 CREATE VIEW Question4DataFinalLucieTrusinova2 AS
 SELECT
-	YEAR,
+	year,
 	avg_price AS current_year_price,
-	LAG(avg_price) OVER (ORDER BY YEAR) AS previous_year_price,
-	avg_price - LAG(avg_price) OVER (ORDER BY YEAR) AS yoy_price_change,
-	ROUND(((avg_price - LAG(avg_price) OVER (ORDER BY YEAR)) * 100.0 / LAG(avg_price) OVER (ORDER BY YEAR)), 2) AS yoy_percent_change_price,
+	LAG(avg_price) OVER (ORDER BY year) AS previous_year_price,
+	avg_price - LAG(avg_price) OVER (ORDER BY year) AS yoy_price_change,
+	ROUND(((avg_price - LAG(avg_price) OVER (ORDER BY year)) * 100.0 / LAG(avg_price) OVER (ORDER BY year)), 2) AS yoy_percent_change_price,
 	avg_wages AS current_year_wages,
-	LAG(avg_wages) OVER (ORDER BY YEAR) AS previous_year_wages, 
-	avg_wages - LAG(avg_wages) OVER (ORDER BY YEAR) AS yoy_wages_change,
-	ROUND(((avg_wages - LAG(avg_wages) OVER (ORDER BY YEAR)) * 100.0 / LAG(avg_wages) OVER (ORDER BY YEAR)), 2) AS yoy_percent_change_wages,
-	ROUND((((avg_price - LAG(avg_price) OVER (ORDER BY YEAR)) * 100.0 / LAG(avg_price) OVER ( ORDER BY YEAR)) - ((avg_wages - LAG(avg_wages) OVER (ORDER BY YEAR)) * 100.0 / LAG(avg_wages) OVER (ORDER BY YEAR))), 2) AS percent_growth_difference
+	LAG(avg_wages) OVER (ORDER BY year) AS previous_year_wages, 
+	avg_wages - LAG(avg_wages) OVER (ORDER BY year) AS yoy_wages_change,
+	ROUND(((avg_wages - LAG(avg_wages) OVER (ORDER BY year)) * 100.0 / LAG(avg_wages) OVER (ORDER BY year)), 2) AS yoy_percent_change_wages,
+	ROUND((((avg_price - LAG(avg_price) OVER (ORDER BY year)) * 100.0 / LAG(avg_price) OVER ( ORDER BY year)) - ((avg_wages - LAG(avg_wages) OVER (ORDER BY year)) * 100.0 / LAG(avg_wages) OVER (ORDER BY year))), 2) AS percent_growth_difference
 FROM
 	question4answerdata_Lucie_Trusinova
 ORDER BY
-	YEAR;
+	year;
 
 
 --Select pro odpověď na otázku 4:
 SELECT
-    YEAR,
+    year,
     yoy_percent_change_price,
     yoy_percent_change_wages,
     percent_growth_difference
