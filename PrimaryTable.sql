dd69d475dac3f4014c6ffff1d068fd03898f64f1
happygolucy <lucie.trusinova@gmail.com>
2026-09-01 21:30:52 +0200

    Update PrimaryTable.sql

--- PrimaryTable.sql
+++ PrimaryTable.sql
@@ -1,14 +1,15 @@
 --tvorba primární tabulky:
 
-CREATE TABLE t_lucie_trusinova_project_sql_primary_final
-(id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-   year INT NOT NULL,   
+CREATE TABLE t_lucie_trusinova_project_sql_primary_final(
+	id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+   	year INT NOT NULL,   
 	food_type TEXT,
-   price_level NUMERIC(12, 2),
-   price_value_per_unit TEXT,
-   industry_branch TEXT,
-   wages NUMERIC(12, 2),
-   source_type TEXT NOT NULL);
+   	price_level NUMERIC(12, 2),
+   	price_value_per_unit TEXT,
+   	industry_branch TEXT,
+   	wages NUMERIC(12, 2),
+   	source_type TEXT NOT NULL
+	);
 
 
 --insert do primary tabulky
@@ -26,13 +27,11 @@ SELECT
 FROM
 	czechia_payroll cpay
 JOIN czechia_price cp
-       ON
-	cpay.payroll_year = DATE_PART('year', cp.date_from)
+    ON cpay.payroll_year = DATE_PART('year', cp.date_from)
 	AND cpay.value_type_code = '5958'
 	AND cp.region_code IS NULL
 JOIN czechia_price_category cpc
-       ON
-	cp.category_code = cpc.code
+    ON cp.category_code = cpc.code
 GROUP BY
 	cpay.payroll_year,
 	cpc.name,
@@ -50,17 +49,14 @@ SELECT
 FROM
 	czechia_payroll cpay
 JOIN czechia_payroll_industry_branch cpib
-       ON
-	cpay.industry_branch_code = cpib.code
+    ON cpay.industry_branch_code = cpib.code
 WHERE
 	cpay.value_type_code = '5958'
 GROUP BY
 	cpay.payroll_year,
 	cpib.name)
-INSERT
-	INTO
-	t_lucie_trusinova_project_sql_primary_final
-	(year,
+INSERT INTO	t_lucie_trusinova_project_sql_primary_final	(
+	year,
 	food_type,
 	price_level,
 	price_value_per_unit,
@@ -68,7 +64,7 @@ INSERT
 	wages,
 	source_type)
 SELECT
-   year,
+   	year,
 	food_type,
 	price_level,
 	price_value_per_unit,
@@ -79,10 +75,9 @@ FROM
 	combined
 WHERE
 	year IN (
-	SELECT
-		year
-	FROM
-		combined
+	SELECT year
+	FROM combined
 	GROUP BY year
-	HAVING COUNT(DISTINCT source_type) = 2)
+	HAVING COUNT(DISTINCT source_type) = 2
+	)
 ORDER BY source_type, year, food_type, industry_branch;
