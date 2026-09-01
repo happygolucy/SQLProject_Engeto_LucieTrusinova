ffe3acb603010acde304c0ca55fabd29eb00405c
happygolucy <lucie.trusinova@gmail.com>
2026-09-01 21:06:16 +0200

    Create Otázka01.sql

---
+++ Otázka01.sql
@@ -0,0 +1,40 @@
+--vytvoření podhledu:
+
+CREATE VIEW data_q1 AS
+SELECT
+	YEAR,
+	industry_branch,
+	wages
+FROM
+	t_lucie_trusinova_project_sql_primary_final
+WHERE
+	wages IS NOT NULL
+ORDER BY
+	industry_branch,
+	YEAR ;
+
+
+--Select pro meziroční procentuální růst/pokles mezd:
+
+SELECT
+    YEAR,
+    industry_branch,
+    ROUND((wages - LAG(wages) OVER (PARTITION BY industry_branch ORDER BY year)) * 100.0 / LAG(wages) OVER (PARTITION BY industry_branch ORDER BY year),2) AS yoy_growth_percent
+FROM data_q1
+ORDER BY
+    industry_branch,
+    YEAR;
+ 
+
+--Select pro celkový růst napříč jednotlivými odvětvími: 
+
+SELECT
+	industry_branch,
+	ROUND((MAX(wages) - MIN(wages)) * 100.0 / MIN(wages), 2) AS total_growth_percent
+FROM
+	data_q1
+GROUP BY
+	industry_branch
+ORDER BY
+	total_growth_percent DESC;
+
